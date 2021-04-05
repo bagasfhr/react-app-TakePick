609c3fc97e0251b4285b70032ca2722cde9a3270
Bagas Windows <bagasfariztanto@gmail.com>
2021-04-05 23:53:18 +0700

    Add file buttonSize, colors, and fonts

---
+++ src/constants/fonts.js
@@ -0,0 +1,12 @@
+const fonts = {
+  poppinsSemiBold: 'Poppins-SemiBold',
+  poppinsRegular: 'Poppins-Regular',
+  poppinsMedium: 'Poppins-Medium',
+  poppinsBold: 'Poppins-Bold',
+  poppinsBlack: 'Poppins-Black',
+  robotoRegular: 'Roboto-Regular',
+  robotoMedium: 'Roboto-Medium',
+  robotoBold: 'Roboto-Bold',
+  robotoBlack: 'Roboto-Black',
+};
+export default fonts;
