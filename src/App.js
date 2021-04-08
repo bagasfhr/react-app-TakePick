b12d375b502b1fb5b111c2eced40ec16b3a7f4cf
Bagas Windows <bagasfariztanto@gmail.com>
2021-04-08 15:26:24 +0700

    Modified .gitignore and App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
-import TestPage from './../src/TestPage';
+import TestPage from './../src/testPage';
 
 const App = () => {
   return (
